af57f3ede48e583dda79ae6a51af171fdf2f0dc0
LakeR <njlaker@gmail.com>
2018-08-09 16:30:07 +0800

    [code] add function name

--- src/Config.js
+++ src/Config.js
@@ -11,7 +11,7 @@ module.exports = {
         titleBarColor = color;
     },
 
-    getTitleBarColor: function () {
+    getTitleBarColor: function getTitleBarColor() {
         return titleBarColor
     }
 };
