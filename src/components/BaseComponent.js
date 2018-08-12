7100a589413f7e2691f6a1f154391e44647b6836
LakeR <njlaker@gmail.com>
2018-08-12 12:23:13 +0800

    [code] support react-native-navigation v2

--- src/components/BaseComponent.js
+++ src/components/BaseComponent.js
@@ -4,6 +4,7 @@
 'use strict';
 
 import React, {Component} from "react";
+import { Navigation } from 'react-native-navigation';
 
 class BaseComponent extends Component {
 
@@ -11,16 +12,16 @@ class BaseComponent extends Component {
     constructor(props) {
         super(props);
         // if you want to listen on navigator events, set this up
-        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+        Navigation.events().bindComponent(this);
     }
 
-    onNavigatorEvent(event) { // this is the onPress handler for the two buttons together
-        if (event.type == 'NavBarButtonPress') { // this is the event type for button presses
-            if ((event.id == 'backButton') || (event.id == 'backPress')) { // this is the same id field from the static navigatorButtons definition
-                this.props.navigator.pop();
-            }
+
+    navigationButtonPressed({ buttonId }) {
+        if(buttonId == 'backButton' || buttonId == 'backPress'){
+            Navigation.pop(this.props.componentId);
         }
     }
+
 
 
 }
--- src/util/NavigatorUtils.js
+++ src/util/NavigatorUtils.js
@@ -1,34 +1,52 @@
 /**
  * Created by zhongxiangyong on 2017/01/01.
  */
-import {Navigation} from "react-native-navigation";
-import Config from "../Config";
+import React from "react";
+import {Provider} from "react-redux";
+
 module.exports = {
     pushWithBack: function pushParam(screen, title, props, rightButtons, navStyle) {
         return ({
-            screen: screen,
-            title: title,
-            navigatorStyle: Object.assign({}, {
-                navBarBackgroundColor: Config.getTitleBarColor(),
-                navBarTextColor: 'white',
-                navBarButtonColor: 'white',
-                tabBarHidden: true,
-                navBarNoBorder: true,
-                topBarElevationShadowEnabled: false,
-                navBarTitleTextCentered: true,
-            }, navStyle),
-            navigatorButtons: {
-                leftButtons: [
-                    {
-                        icon: require('../../img/back_white_button.png'),
-                        id: 'backButton',
-                        title: '返回'
-                    }
-                ],
-                rightButtons: rightButtons
-            },
-            passProps: props
+            component: {
+                name: screen,
+                passProps: props,
+                options: {
+                    topBar: {
+                        title: {
+                            text: title
+                        },
+                        rightButtons: rightButtons == null ? [] : rightButtons,
+                        leftButtons: [{
+                            id: 'backButton',
+                            icon:require('../../img/back_white_button.png')
+                        }],
+                        backButton:{
+                            visible:false,
+                        }
+                    },
+                    bottomTabs: {
+                        visible: false,
+                        drawBehind:true,
+                    },
+                    animate: true,
+                }
+            }
         });
+    },
+
+
+    reduxStoreWrapper: function (MyComponent, store) {
+        return () => {
+            return class StoreWrapper extends React.Component {
+                render() {
+                    return (
+                        <Provider store={store}>
+                            <MyComponent {...this.props}/>
+                        </Provider>
+                    );
+                }
+            };
+        };
     }
 
 };
